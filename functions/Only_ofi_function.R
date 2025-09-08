9bae246b4d1b239ed620d46e97c8b2e8b81ccfb6
VivekKanwar <vivekk@noacs.io>
2025-09-08 16:52:11 +0200

    Writing start of introduction

--- functions/Only_ofi_function.R
+++ functions/Only_ofi_function.R
@@ -3,7 +3,8 @@ library(dplyr)
 
 filter_ofi <- function(x) 
 {
-  dplyr::filter(x, !is.na(ofi) & ofi != "No") 
+  dplyr::filter(x, !is.na(ofi) & ofi != "No") #Filtering patients so that only those where ofi is +ve are left
   }
 
-ofi.data <- filter_ofi(merged.data)
+ofi.data <- filter_ofi(merged.data) #testing if function works 
+
