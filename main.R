14952f5c4aa51cd577f9437e55abe8369575fc51
VivekKanwar <vivekk@k9tbt01.ki.se>
2025-12-07 14:09:35 +0100

    Performing CC filtering before descriptive analysis + comments

--- main.R
+++ main.R
@@ -3,42 +3,44 @@ noacsr::source_all_functions()
 # Load packages
 library(rofi) # installed using remotes::install_github("martingerdin/rofi")
 library(gtsummary)
+library(broom)
 library(tidyverse)
 library(stringr)
 library(DiagrammeR)
 library(rsvg)
 library(flextable)
-# --- Organising raw data ------------------------------------------
 
-# Import data
+# --- Organising raw data -----------------------------------------------------
+
+## ---- Import & merge raw data -----------------------------------------------
 data <- import_data()
 
 # Merge data and remove duplicate variables/columns
-merged.data <- merge_data(data)
+merged.data <- merge_data(data) 
 merged.data <- merged.data[, !duplicated(names(merged.data))]
 
-# Add opportunities for improvement outcome and making it a factor
+## ---- OFI outcome -----------------------------------------------------------
 merged.data$ofi <- create_ofi(merged.data)
+
 merged.data <- merged.data %>%
-  dplyr::mutate(ofi = factor(ofi, levels = c("No","Yes"))) 
+  mutate(ofi = factor(ofi, levels = c("No","Yes"))) 
 
 n_total_merged_data <- nrow(merged.data)
 
+## --- Adding TBI to data --------------------------------------------------
 
-# --- Adding TBI to data --------------------------------------------------
-
-AIS_columns <- grep("^AISCode_", names(merged.data), value = TRUE) #Identifying the columns/variables that have the AIS Code (String)
+#Identifying the columns/variables that have the AIS Code (String)
+AIS_columns <- grep("^AISCode_", names(merged.data), value = TRUE) 
 
 # Adding column to data that indicates whether the patient has TBI or not 
 # And also what severity of TBI
-
 merged.data <- merged.data %>%
   mutate(
     
-    # Get max AIS severity among 140-codes
+    # Get max AIS severity among 1-codes
     TBI_sev_num = apply(select(., all_of(AIS_columns)), 1, Get_TBI_Severity),
     
-    # 2) TBI = had any 140-code
+    # 2) TBI = had any 1-code
     TBI = !is.na(TBI_sev_num),
     
     # Map to categories
@@ -51,18 +53,21 @@ merged.data <- merged.data %>%
     )
   )
 
-# --- Filtering data --------------------------------------------------
+# --- Filtering data ----------------------------------------------------------
 
-# Making dataset with only patients that have TBI
-TBI.only.data <- merged.data %>% filter(TBI)
+## ---- Restrict to TBI patients & derive RTS / On-call -----------------------
+
+# Making dataset with only patients with TBI
+TBI.only.data <- merged.data %>% 
+  filter(TBI)
 n_TBI_only_data <- sum(TBI.only.data$TBI)
 
-# Cant find DOA as a variable, but filter the data so that they are excluded 
-# Also filter patients <18 years 
-
+# Adding On-call hours and RTS as variables 
 TBI.only.data <- TBI.only.data %>%
   Add_OnCall(datetime_col = "DateTime_ArrivalAtHospital") %>%
   Add_RTS()
+
+## ---- Variables we bring into the TBI dataset -------------------------------
 
 Variables_wanted <- c(  
   
@@ -74,11 +79,9 @@ Variables_wanted <- c(
   "inj_dominant",                                                               # Injury type
   "pt_asa_preinjury",                                                           # ASA class
   "host_vent_days_NotDone",                                                     # Mechanical ventilation
-  "iva_dagar_n", "hosp_los_days",                                               # Length of stay 
   "TBI_sev_cat",                                                                # Severiy of TBI
   
   # Continuous 
-  
   "pt_age_yrs",                                                                 # Age
   "ISS", "NISS",                                                                # ISS, NISS
   "ed_sbp_value",                                                               # SBP 
@@ -86,13 +89,17 @@ Variables_wanted <- c(
   "ed_gcs_sum",                                                                 # GCS
   "dt_ed_first_ct",                                                             # Time to first CT
   "RTS",                                                                        # RTS
+  "iva_dagar_n", "hosp_los_days",                                               # Length of stay 
   
   # Outcome 
   "ofi"
 )
 
-# Filtering my dataset to only the variables i want, including RTS and On call. How they are derived can be seen in Variable_filter
+# Filtering my dataset to only the variables i want, including RTS and On call. 
+# How they are derived can be seen in Variable_filter
 TBI.only.filtered <- TBI.only.data %>% select(all_of(Variables_wanted))
+
+##---- Age restriction & missing OFI -----------------------------------------
 
 # Excluding individuals that are under 18
 n_under18 <- sum(TBI.only.filtered$pt_age_yrs < 18) 
@@ -105,11 +112,9 @@ n_no_ofi <- sum(is.na(TBI.only.filtered$ofi))
 
 Analysis.sample <- Remove_missing_ofi(TBI.only.filtered)
 
-# --- Descriptive table -------------------------------------------
+## ---- Organise variables & define predictors --------------------------------
 
-## --- Lists of variables and labels ----------------------------------------
-
-Variables_ordered <- c(
+Variables_ordered <- c( #Could replace variables_wanted?
   # Demographics & baseline
   "Gender", "pt_age_yrs", "pt_asa_preinjury",
   # Type
@@ -126,41 +131,59 @@ Variables_ordered <- c(
   "iva_dagar_n", "hosp_los_days"
 )
 
-# Reusable labels list for descriptive tables
-Labels_table1 <- list(
+## --- Choosing predictors/variables and dropping NA values --------------------
+
+# Exclude variables due to collinearity (NISS, TBI_sev_cat, ED categories)
+# or because they overlap conceptually with other included variables (iva_dagar_n)
+exclude_vars <- c("NISS", "ed_gcs_cat", "ed_sbp_cat", "ed_rr_cat","TBI_sev_cat", "iva_dagar_n")
+
+Complete.analysis.sample <- Analysis.sample %>%
+  Variable_Organiser() %>%
+  select(-all_of(exclude_vars))
+
+# Predictors used in regression models
+Predictors <- Variables_ordered %>%
+  intersect(names(Complete.analysis.sample)) %>%
+  setdiff(exclude_vars)
+
+n_eligible <- nrow(Complete.analysis.sample)
+
+# Complete-case analysis for chosen predictors
+Complete.analysis.sample <- Complete.analysis.sample %>%  
+  filter(if_all(all_of(Predictors), ~ !is.na(.)))
+
+n_final <- nrow(Complete.analysis.sample)
+n_missing_cc <- n_eligible - n_final
+n_ofi <- sum(Complete.analysis.sample$ofi == "Yes")
+
+# --- Descriptive table -------------------------------------------------------
+
+## --- Labels used in the tables ----------------------------------------------
+
+Labels_tables <- list( 
   Gender ~ "Gender",
   pt_age_yrs ~ "Age (years)",
   pt_asa_preinjury ~ "ASA class (preinjury)",
   inj_dominant ~ "Injury type",
-  ed_gcs_cat ~ "GCS in ED",
-  ed_sbp_cat ~ "SBP in ED",
-  ed_rr_cat ~ "RR in ED",
   ISS ~ "Injury Severity Score (ISS)",
-  NISS ~ "New Injury Severity Score (NISS)",
   RTS ~ "Revised Trauma Score (RTS)",
-  TBI_sev_cat ~ "TBI severity",
   Intubation ~ "Intubation",
   dt_ed_first_ct ~ "Time to first CT (min)",
   host_vent_days_NotDone ~ "Mechanical ventilation",
   OnCall ~ "On-call times",
   host_care_level ~ "Hospital care level",
-  iva_dagar_n ~ "ICU length of stay (days)",
   hosp_los_days ~ "Hospital length of stay (days)"
 )
 
-## --- Organising variables -------------------------------------------------
+## --- Producing descriptive table (Table1) ------------------------------------
 
-Descriptive.table1.data <- Analysis.sample  %>% Variable_Organiser()
+Variables_table1 <- c("ofi", Predictors)
 
-## --- Producing descriptive table -----------------------------------------
-
-Variables_table1 <- c("ofi", Variables_ordered)
-
-Descriptive.table1 <- Descriptive.table1.data %>%
-  select(all_of(c("ofi", Variables_table1))) %>%
+Descriptive.table1 <-Complete.analysis.sample %>%
+  select(all_of(Variables_table1)) %>%
   tbl_summary(
     by = ofi,
-    label = Labels_table1,
+    label = Labels_tables,
     missing = "ifany",
     missing_text = "No data",
     statistic = list(
@@ -169,43 +192,20 @@ Descriptive.table1 <- Descriptive.table1.data %>%
     ),
     digits = list(RTS~1) #Rounding RTS to 1 decimal 
   )%>%
+  add_p() %>%
   add_overall() %>%
   bold_labels() %>%  
   modify_caption("**Table 1. Sample characteristics and processes**")
 
 Descriptive.table1 # First baseline descriptive table
 
+# --- Simple regression analysis ----------------------------------------------
 
-# --- Simple regression analysis -----------------------------------
-
-## --- Choosing predictors/variables and dropping NA values --------------------
-
-exclude_vars <- c("NISS", "ed_gcs_cat", "ed_sbp_cat", "ed_rr_cat","TBI_sev_cat", "iva_dagar_n")
-
-Complete.analysis.sample <- Analysis.sample %>%
-  Variable_Organiser() %>%
-  select(-all_of(exclude_vars))
-
-Predictors <- Variables_ordered %>%
-  intersect(names(Complete.analysis.sample)) %>%
-  setdiff(exclude_vars)
-
-n_eligible <- nrow(Complete.analysis.sample)
-
-Complete.analysis.sample <- Complete.analysis.sample %>%  
-  filter(if_all(all_of(Predictors), ~ !is.na(.)))
-
-n_final <- nrow(Complete.analysis.sample)
-
-n_missing_cc <- n_eligible - n_final
-
-n_ofi <- sum(Complete.analysis.sample$ofi == "Yes")
-
-## --- Model that produces simple regression results -------------
+## --- Model that produces unadjusted logistic regression results -------------
 
 #For every variable (Predictors) in the Complete.analysis.sample data, we fit a separate
 #logistic regression model with OFI (Yes/No) as the dependent variable.
-#Each model is stored in the list `SR.model`.
+#Each model is stored in the list `SR.model`
 
 SR.model <- map(Predictors, ~ glm(reformulate(.x, "ofi"),      
                                   data = Complete.analysis.sample,
@@ -213,38 +213,17 @@ SR.model <- map(Predictors, ~ glm(reformulate(.x, "ofi"),
 )   
 
 # Code below loops through both SR.model and predictors in parallel.
-
 SR.results <- map2_dfr(
   SR.model,                #List of fitted glm() models (one per predictor)
   Predictors,              #The matching predictor names (same order as SR.model)
-  ~ broom::tidy(.x,        
+  ~ tidy(.x,        
                 conf.int = TRUE,            #Add 95% CIs
                 exponentiate = TRUE) %>%    #Turn log-odds into ORs 
     filter(term != "(Intercept)") %>%       #Removing Intercept as the baseline OR is not relevant 
     mutate(variable = .y)                   #Add the predictor name (from .y) as a column
 )
 
-## --- Producing simple regression table --------------------------------------
-
-### --- Labels used in the regression tables ----------------------------------
-
-Labels_regression_tables <- list(
-  Gender ~ "Gender",
-  pt_age_yrs ~ "Age (years)",
-  pt_asa_preinjury ~ "ASA class (preinjury)",
-  inj_dominant ~ "Injury type",
-  ISS ~ "Injury Severity Score (ISS)",
-  RTS ~ "Revised Trauma Score (RTS)",
-  Intubation ~ "Intubation",
-  dt_ed_first_ct ~ "Time to first CT (min)",
-  host_vent_days_NotDone ~ "Mechanical ventilation",
-  OnCall ~ "On-call times",
-  host_care_level ~ "Hospital care level",
-  # iva_dagar_n ~ "ICU length of stay (days)",
-  hosp_los_days ~ "Hospital length of stay (days)"
-)
-
-### --- Constructing table ---------------------------------------------------
+## --- Producing unadjusted logistic regression table -------------------------
 
 SR.Table1 <- tbl_uvregression(
   data = Complete.analysis.sample,
@@ -257,7 +236,7 @@ SR.Table1 <- tbl_uvregression(
   conf.int = TRUE,
   conf.level = 0.95,
   pvalue_fun = label_style_pvalue(digits = 3),
-  label = Labels_regression_tables, 
+  label = Labels_tables, 
   missing = "ifany",
   missing_text = "No data"
 ) %>%
@@ -287,27 +266,24 @@ SR.Table1 <- tbl_uvregression(
     "**Table 2. Unadjusted logistic regression analyses of associations between patient level factors 
 and opportunities for improvement in TBI patients**")
 
+# SR.Table1 
 
-SR.Table1 # Table describing unadjusted associations 
-
-# --- Multivariable regression analysis ---------------------------------------
+# --- Multivariable logistic regression ---------------------------------------
 
 ## --- Building the MV model --------------------------------------------------
 
 # Producing code that joins all variable names in the predictor vector into a single string separated by +
 # Adding the ofi at the start and then converting it into a formula to fit in the regression model glm(formula, data, family)
-
 MV.formula <- as.formula(paste("ofi ~", paste(Predictors, collapse = " + ")))
 
 # Building the multivariable model with MV.formula containing ofi and all predictors
-
-MV.model <- glm(MV.formula, 
-                data = Complete.analysis.sample,
-                family = binomial)
+MV.model <- glm(
+  MV.formula, 
+  data = Complete.analysis.sample,
+  family = binomial)
 
 # Tidying results as OR with 95% CI
-
-MV.results <- broom::tidy(
+MV.results <- tidy(
   MV.model,
   exponentiate = TRUE,
   conf.int = TRUE,
@@ -316,10 +292,7 @@ MV.results <- broom::tidy(
 
 ## --- Producing multivariable table ------------------------------------------
 
-# Producing table that displays the adjusted ORs for the predictors using tbl_regression() 
-# Adding 95% CI and P value with 3 decimals to table
-# Also changing names with modify_header and label 
-
+# Producing table that displays the adjusted ORs for the predictors 
 MV.Table1 <- tbl_regression(
   x = MV.model,
   exponentiate = TRUE,
@@ -327,7 +300,7 @@ MV.Table1 <- tbl_regression(
   conf.int = TRUE,
   include = all_of(Predictors),
   pvalue_fun = label_style_pvalue(digits = 3),
-  label = Labels_regression_tables, # For real data use Labels_table1
+  label = Labels_tables, # For real data use Labels_table1
   missing = "ifany",
   missing_text = "No data"
 ) %>%
@@ -357,64 +330,15 @@ MV.Table1 <- tbl_regression(
     "**Table 3. Adjusted logistic regression analyses of associations between patient level factors 
 and opportunities for improvement in TBI patients**"
   )
+# MV.Table1 # Table describing adjusted associations
 
-MV.Table1 # Table describing adjusted associations
-
-
-# --- Merging SR and MV tables -----------------------------------------------
+# -----Combined unadjusted & adjusted table (Table2) --------------------------
 
 Merged.Table <- tbl_merge(
-  tbls = list(SR.Table1, MV.Table1)
+  tbls = list(SR.Table1, MV.Table1),
+  tab_spanner = c("**Unadjusted**", "**Adjusted**")
 ) %>%
-  modify_table_body(
-    ~ .x %>%
-      mutate(
-        # Unadjusted OR (CI)
-        OR_CI_1 = if_else(
-          !is.na(estimate_1) & !is.na(conf.low_1) & !is.na(conf.high_1),
-          paste0(
-            sprintf("%.2f", estimate_1), " (",
-            sprintf("%.2f", conf.low_1), ", ",
-            sprintf("%.2f", conf.high_1), ")"
-          ),
-          NA_character_
-        ),
-        # Adjusted OR (CI)
-        OR_CI_2 = if_else(
-          !is.na(estimate_2) & !is.na(conf.low_2) & !is.na(conf.high_2),
-          paste0(
-            sprintf("%.2f", estimate_2), " (",
-            sprintf("%.2f", conf.low_2), ", ",
-            sprintf("%.2f", conf.high_2), ")"
-          ),
-          NA_character_
-        )
-      ) %>%
-      # Final visible column order
-      select(label, OR_CI_1, p.value_1, OR_CI_2, p.value_2,everything())
-  ) %>%
-  # Hide old numeric columns
-  modify_column_hide(
-    columns = c(
-      estimate_1, conf.low_1, conf.high_1,
-      estimate_2, conf.low_2, conf.high_2
-    )
-  ) %>%
-  # Set column headers
-  modify_header(
-    label     = "**Characteristics/Predictors**",
-    OR_CI_1   = "**OR (95% CI)**",
-    p.value_1 = "**p-value**",
-    OR_CI_2   = "**OR (95% CI)**",
-    p.value_2 = "**p-value**"
-  ) %>%
-  # Now explicitly define the spanners
-  modify_spanning_header(
-    c("OR_CI_1", "p.value_1") ~ "**Unadjusted**",
-    c("OR_CI_2", "p.value_2") ~ "**Adjusted**"
-  ) %>%
   modify_caption(
-    "**Table 4. Unadjusted and adjusted logistic regression analyses of associations between patient-level factors and opportunities for improvement in TBI patients**"
+    "**Table 2. Unadjusted and adjusted logistic regression analyses of associations between patient-level factors and opportunities for improvement in TBI patients**"
   )
-
 Merged.Table
